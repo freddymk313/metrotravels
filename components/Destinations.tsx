dc194fbf9541d97bbf33c4abf4efe5de53d7bb3b
freddymk313 <107887953+freddymk313@users.noreply.github.com>
2025-07-04 18:34:58 +0000

    fixing maroc img

--- components/Destinations.tsx
+++ components/Destinations.tsx
@@ -84,7 +84,7 @@ const destinations = [
     title: "Maroc",
     subtitle: "Désert et médinas",
     price: "$750",
-    image: "/img/Maroc.png",
+    image: "/img/maroc.png",
     description: "Des souks colorés de Marrakech aux dunes du Sahara, une expérience sensorielle unique.",
     rating: 4.5,
     featured: false,
